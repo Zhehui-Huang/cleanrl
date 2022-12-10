6f4220f5a8dcf280a503a2f5107d2ce4ad6c20c1
Zhehui Huang <zhehuihu@usc.edu>
2022-12-09 17:17:09 -0800

    Add training bash

---
+++ benchmark/slurm/bash_train_mujoco_pendulum.sh
@@ -0,0 +1,8 @@
+poetry install --with mujoco,pybullet
+poetry run python -c "import mujoco_py"
+xvfb-run -a poetry run python -m sample_factory.launcher.run \
+--run=benchmark.slurm.train_mujoco --backend=slurm \
+--slurm_workdir=slurm_output \
+--slurm_gpus_per_job=1 --slurm_cpus_per_gpu=16 \
+ --experiment_suffix=slurm --slurm_sbatch_template=/home/zhehui/reward_decrease/dir_cleanrl/sbatch_timeout.sh \
+ --slurm_print_only=False
