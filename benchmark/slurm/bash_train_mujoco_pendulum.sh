62097a4546f0fdc45a356b38a07498e024594cf9
Zhehui Huang <zhehuihu@usc.edu>
2022-12-09 17:24:35 -0800

    Minor modifications

--- benchmark/slurm/bash_train_mujoco_pendulum.sh
+++ benchmark/slurm/bash_train_mujoco_pendulum.sh
@@ -1,5 +1,5 @@
 xvfb-run -a python -m sample_factory.launcher.run \
---run=benchmark.slurm.train_mujoco --backend=slurm \
+--run=benchmark.slurm.train_mujoco_pendulum --backend=slurm \
 --slurm_workdir=slurm_output \
 --slurm_gpus_per_job=1 --slurm_cpus_per_gpu=16 \
  --experiment_suffix=slurm --slurm_sbatch_template=/home/zhehui/reward_decrease/dir_cleanrl/sbatch_timeout.sh \
