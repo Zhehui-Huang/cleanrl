6d1204345290056c9bfbbb50efceccddc19c4f87
Zhehui Huang <zhehuihu@usc.edu>
2022-12-09 17:48:04 -0800

    Minor modifications

--- benchmark/slurm/train_mujoco_pendulum.py
+++ benchmark/slurm/train_mujoco_pendulum.py
@@ -6,7 +6,7 @@ MUJOCO_BASELINE = (
     '--total-timesteps=100000000 --num-envs=64 --anneal-lr=True --clip-vloss=False --ent-coef=0 '
     '--learning-rate=0.00295 --max-grad-norm=3.5 --num-minibatches=4 --num-steps=64 --update-epochs=2 --vf-coef=1.3 '
     '--track --capture-video --wandb-project-name mujoco-cleanrl '
-    ' --wandb-entity=multi-drones --wandb-project-name=zh-reward-decrease'
+    '--wandb-entity=multi-drones --wandb-project-name=zh-reward-decrease'
 )
 
 
